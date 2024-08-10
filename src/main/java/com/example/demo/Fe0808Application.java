8700dd375ed4d1958b45136e949a1aaf921b1dcf
hey5nee123 <gpdnjsl123@naver.com>
2024-08-10 11:31:17 +0900

    dddd

---
+++ src/main/java/com/example/demo/Fe0808Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fe0808Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fe0808Application.class, args);
+	}
+
+}
